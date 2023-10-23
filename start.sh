2bb04b0b551302ce6fd8b6062f8fdec6f88cd0f1
Cap-n-Proud <kingsta@protonmail.com>
2023-10-23 21:24:43 +0200

    upd

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
-# Process images in the InstantUPload folder
-python3 /app/controller.py -iw /mnt/Photos/000-InstantUpload -s /mnt/secrets/imageflow -c /mnt/imageflow/config
+# Process images in the InstantUpload folder
+python3 /app/controller.py --moveFileImage True -id "/mnt/Photos/005-PhotoBook/" -iw "/mnt/Photos/000-InstantUpload" -s /mnt/secrets/imageflow -c /mnt/imageflow/config -fc "/mnt/Apps_Config/imageflow/faceClassifier.pkl"
+
+# Produced Videos are proecessed but not moved
+python3 /app/controller.py --moveFileImage False -id "/mnt/Photos/010-Videos/000-Produced/" -iw "/mnt/Photos/010-Videos/000-Produced/" -s /mnt/secrets/imageflow -c /mnt/imageflow/config -fc "/mnt/Apps_Config/imageflow/faceClassifier.pkl"
 
 sleep infinity
